c7d82f31cf8946a1303c9eede15de97168fcad37
David Baetge <david.baetge@gmail.com>
2022-06-19 16:41:20 +0200

    Updated invalidations.

--- admin_scripts/invalidate-12hourly.sh
+++ admin_scripts/invalidate-12hourly.sh
@@ -3,4 +3,10 @@
 # Parameters
 # $1 - The AWS cloudfront distribution ID.
 
+if [[ $# -lt 1 ]] ; then
+  echo "Wrong number of arguments!"
+  echo "Usage: ./invalidate-12hourly.sh <CF DISTRIBUTION ID>"
+  exit 1
+fi
+
 aws cloudfront create-invalidation --distribution-id "$1" --paths "/*"
--- admin_scripts/invalidate-hourly.sh
+++ admin_scripts/invalidate-hourly.sh
@@ -3,5 +3,11 @@
 # Parameters
 # $1 - The AWS cloudfront distribution ID.
 
+if [[ $# -lt 1 ]] ; then
+  echo "Wrong number of arguments!"
+  echo "Usage: ./invalidate-hourly.sh <CF DISTRIBUTION ID>"
+  exit 1
+fi
+
 aws cloudfront create-invalidation --distribution-id "$1"\
     --paths "/month*" "/year*" "/week.html"
