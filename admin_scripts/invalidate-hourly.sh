c5e045f9f8dd32b22351a564d5ff5050d6b5ce38
David Baetge <david.baetge@gmail.com>
2022-10-22 00:41:33 +0200

    Updated invalidations.

--- admin_scripts/invalidate-hourly.sh
+++ admin_scripts/invalidate-hourly.sh
@@ -10,4 +10,4 @@ if [[ $# -lt 1 ]] ; then
 fi
 
 aws cloudfront create-invalidation --distribution-id "$1"\
-    --paths "/month*" "/year*" "/week.html" "/dwd/bwk_bodendruck_na_ana.png" "/dwd/bwk_bodendruck_weu_ana.png" "/dwd/SchilderLZ.jpg" "/dwd.html"
+    --paths "/yesterday.html" "/week.html" "/month.html" "/year.html" "/dwd/bwk_bodendruck_na_ana.png" "/dwd/bwk_bodendruck_weu_ana.png" "/dwd/SchilderLZ.jpg" "/dwd.html"
